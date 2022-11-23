ea0dd14c64cd26c99b853bbc6b88a9164963161c
sty <shitianyu7777@gmail.com>
2022-11-23 21:14:55 +0800

    add 帖子详情

--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -41,4 +41,9 @@ public class DiscussPostService {
         return discussPostMapper.insertDiscussPost(post);
     }
 
+    public DiscussPost findDiscussPostById(int id) {
+        return discussPostMapper.selectDiscussPostById(id);
+    }
+
+
 }
