a3abac5e505f7149fb818fbfe1e684ca0d5f1f1a
sty <shitianyu7777@gmail.com>
2022-11-24 20:22:53 +0800

    add 添加评论

--- src/main/java/com/nowcoder/community/dao/CommentMapper.java
+++ src/main/java/com/nowcoder/community/dao/CommentMapper.java
@@ -1,6 +1,7 @@
 package com.nowcoder.community.dao;
 
 import com.nowcoder.community.entity.Comment;
+import com.nowcoder.community.entity.DiscussPost;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -11,5 +12,7 @@ public interface CommentMapper {
     List<Comment> selectCommentsByEntity(int entityType, int entityId, int offset, int limit);
 
     int selectCountByEntity(int entityType, int entityId);
+
+    int insertComment(Comment comment);
 
 }
